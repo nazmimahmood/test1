6a4252aeaa153ccf48f66eef2d10b7b45f8ca468
apatkat <nazmi.mahmood@gmail.com>
2020-07-13 01:35:34 +0900

    add something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # def validate_user
+  #   if user.exist?
+  #     pp "user login"
+  #   else
+  #     "validation fail"
+  #     #error
+  #   end
+  # end
+  #
+  # if Rails.env.production?
+  #   validate_user
+  # end pp {}
 end
